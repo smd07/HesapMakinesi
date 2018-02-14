197975f645f85fe5f297c66376e0c0ae8ed3b2c5
smd07 <samed__1998@hotmail.com>
2018-02-14 21:37:03 +0300

    Add files via upload

---
+++ HesapMakinesi/HesapMakinesi/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HesapMakinesi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void buttonDivide_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonMod_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonMinusOrPlus_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonDEL_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonC_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonMinus_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonPlus_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonEqual_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonPoint_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button0_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
