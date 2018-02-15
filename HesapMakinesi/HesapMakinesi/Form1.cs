81679f843014977447fae8a0ee1353f37a8eed70
fatmanurafyoncu <fafyoncu15@posta.pau.edu.tr>
2018-02-15 23:21:12 +0300

    Update Form1.cs

--- HesapMakinesi/HesapMakinesi/Form1.cs
+++ HesapMakinesi/HesapMakinesi/Form1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -16,15 +16,17 @@ namespace HesapMakinesi
         {
             InitializeComponent();
         }
-        
-        
-        
+        int toplam=0;
+        int sayi1;
+        int sayi2;
+        string isaret;
+
         int sonuc;
         public void Sonuc()
         {
             if (isaret.Equals("+"))
             {
-               sonuc= sayi1 + sayi2;
+                sonuc = sayi1 + sayi2;
                 textBox1.Text = sonuc.ToString();
             }
             else if (isaret.Equals("-"))
@@ -38,7 +40,7 @@ namespace HesapMakinesi
                 sonuc = sayi1 / sayi2;
                 textBox1.Text = sonuc.ToString();
             }
-            else if (isaret.Equals("*"))
+            else if (isaret.Equals("X"))
             {
                 sonuc = sayi1 * sayi2;
                 textBox1.Text = sonuc.ToString();
@@ -48,20 +50,9 @@ namespace HesapMakinesi
                 sonuc = sayi1 % sayi2;
                 textBox1.Text = sonuc.ToString();
             }
-           
+
         }
-        
-        
-        
-        
-        
-        
-        
-        
-        int sayi1;
-        int sayi2;
-        string isaret;
-        
+
         public void isrt(int sayi1,int sayi2)
         {
             if (isaret.Equals("-"))
@@ -70,7 +61,7 @@ namespace HesapMakinesi
             }
             else if (isaret.Equals("+"))
             {
-                //topla(sayi1, sayi2);
+                topla(sayi1, sayi2);
             }
             else if (isaret.Equals("/"))
             {
@@ -78,7 +69,7 @@ namespace HesapMakinesi
             }
             else if (isaret.Equals("X"))
             {
-               // carp(sayi1, sayi2);
+                carp(sayi1, sayi2);
             }
             else
             {
@@ -86,33 +77,46 @@ namespace HesapMakinesi
             }
 
         }
-        
-        
-
+       
+     
+        public void carp(int sayi1, int sayi2)
+        {
+            toplam = sayi1 * sayi2;
+            textBox1.Text = toplam.ToString();
+            this.sayi1 = toplam;
+        }
+       
+        public void topla(int sayi1, int sayi2)
+        {
+            toplam = sayi1 + sayi2;
+            textBox1.Text = toplam.ToString();
+            this.sayi1 = toplam;
+        }
         public void böl(int sayi1, int sayi2)
         {
-            sonuc = sayi1 / sayi2;
-            textBox1.Text = sonuc.ToString();
-            this.sayi1 = sonuc;
+            toplam = sayi1 / sayi2;
+            textBox1.Text = toplam.ToString();
+            this.sayi1 = toplam;
         }
         public void cıkar(int sayi1,int sayi2)
         {
-            sonuc = sayi1 - sayi2;
-            textBox1.Text = sonuc.ToString();
-            this.sayi1 = sonuc;
+            toplam= sayi1 - sayi2;
+            textBox1.Text = toplam.ToString();
+            this.sayi1 = toplam;
 
         }
         public void modal(int sayi1, int sayi2)
         {
-            sonuc = sayi1 % sayi2;
-            textBox1.Text = sonuc.ToString();
-            this.sayi1 = sonuc;
+            toplam = sayi1 % sayi2;
+            textBox1.Text = toplam.ToString();
+            this.sayi1 = toplam;
 
         }
+     
 
         private void buttonDivide_Click(object sender, EventArgs e)
         {
-          label1.Text = label1.Text + "*";
+            label1.Text = label1.Text + "/";
             if (sayi1 == 0)
             {
                 sayi1 = int.Parse(textBox1.Text);
@@ -124,62 +128,38 @@ namespace HesapMakinesi
                 sayi2 = int.Parse(textBox1.Text);
                 textBox1.Text = "";
                 isrt(sayi1, sayi2);
+                label1.Text = sayi1.ToString() + "/";
                 isaret = "/";
             }
         }
 
         private void buttonMod_Click(object sender, EventArgs e)
         {
-         label1.Text = label1.Text + "%";
-             if (sayi1 == 0)
+            label1.Text = label1.Text + "%";
+            if (sayi1 == 0)
             {
                 sayi1 = int.Parse(textBox1.Text);
                 textBox1.Text = "";
                 isaret = "%";
-                sonuc = sayi1;
+                toplam = sayi1;
             }
             else
             {
                 sayi2 = int.Parse(textBox1.Text);
                 textBox1.Text = "";
                 isrt(sayi1, sayi2);
+                label1.Text = sayi1.ToString() + "%";
                 isaret = "%";
             }
 
-        }
-
-        private void buttonMinusOrPlus_Click(object sender, EventArgs e)
-        {
 
         }
 
-        private void buttonDEL_Click(object sender, EventArgs e)
-        {
-        
-        if (textBox1.Text.Length == 0 && label1.Text.ToString().Length>=1)
-            {
-                label1.Text = label1.Text.Substring(0, label1.Text.Length - 1);
-                sayi1 = 0;
-            }
-              
-            else
-                textBox1.Text = textBox1.Text.Substring(0, textBox1.Text.Length - 1);
-                label1.Text = label1.Text.Substring(0, label1.Text.Length - 1);
-
-        }
-
-        private void buttonC_Click(object sender, EventArgs e)
-        {
-            textBox1.Text = "0";
-            sayi1 = 0;
-            sayi2 = 0;
-            isaret = "";
-        }
 
         private void buttonMinus_Click(object sender, EventArgs e)
         {
-         label1.Text = label1.Text + "-";
-        if (sayi1 == 0)
+            label1.Text = label1.Text + "-";
+            if (sayi1 == 0)
             {
                 sayi1 = int.Parse(textBox1.Text);
                 textBox1.Text = "";
@@ -190,65 +170,108 @@ namespace HesapMakinesi
                 sayi2 = int.Parse(textBox1.Text);
                 textBox1.Text = "";
                 isrt(sayi1, sayi2);
+                label1.Text = sayi1.ToString() + "-";
                 isaret = "-";
             }
-
+            
         }
 
         private void buttonPlus_Click(object sender, EventArgs e)
         {
-              label1.Text =label1.Text + "+";
-            if (sayi1==0)
+            label1.Text = label1.Text + "+";
+            if (sayi1 == 0)
             {
                 sayi1 = int.Parse(textBox1.Text);
-                isaret = "+";
                 textBox1.Text = "";
+                isaret = "+";
             }
-            else 
+            else
             {
                 sayi2 = int.Parse(textBox1.Text);
-                Sonuc();
-                sayi1 = sonuc;
-                label1.Text =sayi1.ToString()+"+";
+                textBox1.Text = "";
+                isrt(sayi1, sayi2);
+                label1.Text = sayi1.ToString() + "+";
                 isaret = "+";
             }
+          
+        }
+        private void buttonCross_Click(object sender, EventArgs e)
+        {
+            label1.Text = label1.Text + "X";
 
+            if (sayi1 == 0)
+            {
+                sayi1 = int.Parse(textBox1.Text);
+                textBox1.Text = "";
+                isaret = "X";
+            }
+            else
+            {
+                sayi2 = int.Parse(textBox1.Text);
+                textBox1.Text = "";
+                isrt(sayi1, sayi2);
+                label1.Text = sayi1.ToString() + "X";
+                isaret = "X";
+            }
+        }
+        private void buttonKup_Click(object sender, EventArgs e)
+        {
+           
+            int sayi =int.Parse(textBox1.Text);
+            double toplam = sayi*sayi*sayi;
+            textBox1.Text = toplam.ToString();
+            label1.Text = toplam.ToString();
+        }
+        private void buttonC_Click(object sender, EventArgs e)
+        {
+
+            textBox1.Text = "0";
+            sayi1 = 0;
+            sayi2 = 0;
+            isaret = "";
+            label1.Text = "";
         }
 
         private void buttonEqual_Click(object sender, EventArgs e)
         {
-         if (sayi1 == 0)
+            if (sayi1 == 0)
             {
                 sayi1 = int.Parse(textBox1.Text);
-             
+
                 textBox1.Text = "";
             }
-            else 
+            else
             {
                 sayi2 = int.Parse(textBox1.Text);
                 Sonuc();
                 label1.Text += "=" + sonuc;
                 sayi1 = sonuc;
-               
+
             }
             sayi1 = 0;
             sayi2 = 0;
             isaret = "";
             label1.Text = "";
-
         }
-        
+
        
 
-        private void buttonPoint_Click(object sender, EventArgs e)
+        private void buttonDEL_Click(object sender, EventArgs e)
         {
 
+
+            if (textBox1.Text.Length == 1)
+                textBox1.Text = "0";
+            else
+                textBox1.Text = textBox1.Text.Substring(0, textBox1.Text.Length - 1);
+            label1.Text = label1.Text.Substring(0, label1.Text.Length - 1);
         }
+      
 
         private void button0_Click(object sender, EventArgs e)
         {
             label1.Text += "0";
-            if (textBox1.Text != "" && !textBox1.Text.Equals("0") && int.Parse(textBox1.Text.ToString()) != sonuc)
+            if (textBox1.Text != "" && !textBox1.Text.Equals("0") && int.Parse(textBox1.Text.ToString()) != toplam)
                 textBox1.Text += "0";
             else
                 textBox1.Text = "0";
@@ -257,7 +280,7 @@ namespace HesapMakinesi
         private void button9_Click(object sender, EventArgs e)
         {
             label1.Text += "9";
-            if (textBox1.Text != "" && !textBox1.Text.Equals("0") && int.Parse(textBox1.Text.ToString()) != sonuc)
+            if (textBox1.Text != "" && !textBox1.Text.Equals("0") && int.Parse(textBox1.Text.ToString()) != toplam)
                 textBox1.Text += "9";
             else
                 textBox1.Text = "9";
@@ -266,7 +289,7 @@ namespace HesapMakinesi
         private void button8_Click(object sender, EventArgs e)
         {
             label1.Text += "8";
-            if (textBox1.Text != "" && !textBox1.Text.Equals("0") && int.Parse(textBox1.Text.ToString()) != sonuc)
+            if (textBox1.Text != "" && !textBox1.Text.Equals("0") && int.Parse(textBox1.Text.ToString()) != toplam)
                 textBox1.Text += "8";
             else
                 textBox1.Text = "8";
@@ -275,7 +298,7 @@ namespace HesapMakinesi
         private void button7_Click(object sender, EventArgs e)
         {
             label1.Text += "7";
-            if (textBox1.Text != "" && !textBox1.Text.Equals("0") && int.Parse(textBox1.Text.ToString()) != sonuc)
+            if (textBox1.Text != "" && !textBox1.Text.Equals("0") && int.Parse(textBox1.Text.ToString()) != toplam)
                 textBox1.Text += "7";
             else
                 textBox1.Text = "7";
@@ -284,7 +307,7 @@ namespace HesapMakinesi
         private void button6_Click(object sender, EventArgs e)
         {
             label1.Text += "6";
-            if (textBox1.Text != "" && !textBox1.Text.Equals("0") && int.Parse(textBox1.Text.ToString()) != sonuc)
+            if (textBox1.Text != "" && !textBox1.Text.Equals("0") && int.Parse(textBox1.Text.ToString()) != toplam)
                 textBox1.Text += "6";
             else
                 textBox1.Text = "6";
@@ -293,7 +316,7 @@ namespace HesapMakinesi
         private void button5_Click(object sender, EventArgs e)
         {
             label1.Text += "5";
-            if (textBox1.Text != "" && !textBox1.Text.Equals("0") && int.Parse(textBox1.Text.ToString()) != sonuc)
+            if (textBox1.Text != "" && !textBox1.Text.Equals("0") && int.Parse(textBox1.Text.ToString()) != toplam)
                 textBox1.Text += "5";
             else
                 textBox1.Text = "5";
@@ -302,7 +325,7 @@ namespace HesapMakinesi
         private void button4_Click(object sender, EventArgs e)
         {
             label1.Text += "4";
-            if (textBox1.Text != "" && !textBox1.Text.Equals("0") && int.Parse(textBox1.Text.ToString()) != sonuc)
+            if (textBox1.Text != "" && !textBox1.Text.Equals("0") && int.Parse(textBox1.Text.ToString()) != toplam)
                 textBox1.Text += "4";
             else
                 textBox1.Text = "4";
@@ -311,7 +334,7 @@ namespace HesapMakinesi
         private void button3_Click(object sender, EventArgs e)
         {
             label1.Text += "3";
-            if (textBox1.Text != "" && !textBox1.Text.Equals("0") && int.Parse(textBox1.Text.ToString()) != sonuc)
+            if (textBox1.Text != "" && !textBox1.Text.Equals("0") && int.Parse(textBox1.Text.ToString()) != toplam)
                 textBox1.Text += "3";
             else
                 textBox1.Text = "3";
@@ -320,40 +343,21 @@ namespace HesapMakinesi
         private void button2_Click(object sender, EventArgs e)
         {
             label1.Text += "2";
-            if (textBox1.Text != "" && !textBox1.Text.Equals("0") && int.Parse(textBox1.Text.ToString()) != sonuc)
+            if (textBox1.Text != "" && !textBox1.Text.Equals("0") && int.Parse(textBox1.Text.ToString()) != toplam)
                 textBox1.Text += "2";
             else
                 textBox1.Text = "2";
         }
 
-
-        private void buttonCross_Click(object sender, EventArgs e)
-        {
-            label1.Text = label1.Text + "*";
-            if (sayi1 == 0)
-            {
-                sayi1 = int.Parse(textBox1.Text);
-                isaret = "*";
-                textBox1.Text = "";
-            }
-            else
-            {
-                sayi2 = int.Parse(textBox1.Text);
-                Sonuc();
-
-                sayi1 = sonuc;
-                label1.Text = sayi1.ToString() + "*";
-                isaret = "*";
-            }
-        }
-
         private void button1_Click(object sender, EventArgs e)
         {
             label1.Text += "1";
-            if (textBox1.Text != "" && !textBox1.Text.Equals("0") && int.Parse(textBox1.Text.ToString()) != sonuc)
+            if (textBox1.Text != "" && !textBox1.Text.Equals("0") && int.Parse(textBox1.Text.ToString()) != toplam)
                 textBox1.Text += "1";
             else
                 textBox1.Text = "1";
         }
+
+       
     }
 }
